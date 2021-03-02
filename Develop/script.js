02e0ea5268d507fd5e99d24aa5d48be30c42651d
Abraham Kim <krnpride730@yahoo.com>
2021-03-02 11:58:27 -0500

    added functions that return numbers 0-9 and a random symbol

--- Develop/script.js
+++ Develop/script.js
@@ -11,12 +11,26 @@
 
 // // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
+
+
+// This function generates random lower case letters
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
+//This function generates random upper case letters
 function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
-console.log(getRandomLower());
+// This function generates random numbers 0-9
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+// This function generates random symbols 
+function getRandomSymbol() {
+  const symbols = '!@#$%^&*(){}[]=></,.';
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+console.log(getRandomSymbol());
