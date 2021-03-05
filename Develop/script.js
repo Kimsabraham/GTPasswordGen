bda31d3bae75661f2e7b81182c7eeefba5f592ed
Abraham Kim <krnpride730@yahoo.com>
2021-03-05 15:34:43 -0500

    added some logic where password must be at least 8 chracters

--- Develop/script.js
+++ Develop/script.js
@@ -1,115 +1,107 @@
-// // Assignment Code
-// var generateBtn = document.querySelector("#generate");
-
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-// }
-
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-
-// Array of special chracters 
-var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
+// Array of special chracters
+var symbols = [
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
   "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
 ];
 // Array of numbers
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-//Array of lowercase
-var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+// Array of lowercase
+var lower = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
 ];
-// Array of uppercase
-var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
+// Array of uppercase characters
+var upper = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
 ];
-
 // prompt asking user how long they would like their password to be, using parseInt to make sure we get a integer number
 function getPasswordOptions() {
   var length = parseInt(
-    prompt('How long would you like your password to be?')
+    prompt("How many characters would you like your password to contain?")
   );
-
-// Function to prompt user for password options
-function getPasswordOptions() {
-  var length = parseInt(
-    prompt('How long would you like your password to be?')
-  );
-
+  // Function to prompt user for password options
+  if (isNaN(length) === true) {
+    alert("Password length must be a number!");
+    return;
+  }
+  
+  if (length < 8) {
+    alert("Password length must be at least 8 characters");
+    return;
+  }
+ 
+  if (length > 128) {
+    alert("Password length must less than 129 characters");
+    return;
+  }
  
