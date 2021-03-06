49c5d4f938483d8f23b79aabf1c89e2ec72f6f97
Abraham Kim <krnpride730@yahoo.com>
2021-03-05 19:51:38 -0500

    added logic for chracter lengths and password confirm options

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-// Array of special chracters
+//array of symbols
 var symbols = [
   "@",
   "%",
@@ -24,6 +24,7 @@ var symbols = [
   "_",
   ".",
 ];
+
 // Array of numbers
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 // Array of lowercase
@@ -55,7 +56,7 @@ var lower = [
   "y",
   "z",
 ];
-// Array of uppercase characters
+// Array of uppercase
 var upper = [
   "A",
   "B",
@@ -84,24 +85,30 @@ var upper = [
   "Y",
   "Z",
 ];
-// prompt asking user how long they would like their password to be, using parseInt to make sure we get a integer number
+// prompt asking user how long they would like their password to be, using parseInt to make sure we get a integer as a input
 function getPasswordOptions() {
   var length = parseInt(
     prompt("How many characters would you like your password to contain?")
   );
-  // Function to prompt user for password options
+  // Conditional statement to check if password length is a number, if false loop ends.
   if (isNaN(length) === true) {
-    alert("Password length must be a number!");
+    alert("Password length must be provided as a number");
     return;
   }
-  
+  // statment that makes sure input is at least 8 or more chracters 
   if (length < 8) {
     alert("Password length must be at least 8 characters");
     return;
   }
- 
+  // statment makes sure the input is less then 129 chracters
   if (length > 128) {
     alert("Password length must less than 129 characters");
     return;
   }
+
+  //true/false statments to confirm password input choices
+  var symbols = confirm("Click OK to confirm including special characters.");
+  var numbers = confirm("Click OK to confirm including numeric characters.");
+  var lower = confirm("Click OK to confirm including lowercase characters.");
+  var upper = confirm("Click OK to confirm including uppercase characters.");
  
